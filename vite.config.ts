d9a590fc9fb0e8179a3ffd4f41bb1ffc91479ba6
mintsweet <0x1304570@gmail.com>
2025-04-18 20:00:46 +1200

    feat: update something about library

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,12 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'my-component',
-      fileName: 'my-component',
+      name: 'mints-ui',
+      fileName: 'mints-ui',
     },
     rollupOptions: {
       external: [
+        'react/jsx-runtime',
         ...Object.keys(dependencies),
         ...Object.keys(peerDependencies),
       ],
