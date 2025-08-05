4cfc84ce9b9223de648ea821efbb7d4b509b0689
mintsweet <0x1304570@gmail.com>
2025-08-06 10:15:17 +1200

    refactor(storybook): remove unused dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@chromatic-com/storybook',
-  ],
+  addons: ['@storybook/addon-docs', '@chromatic-com/storybook'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
